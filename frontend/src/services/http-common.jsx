b88e21d1de225e60757494436d343556cb6e4960
sametbakar <sametcb.30@gmail.com>
2024-12-27 18:00:32 +0300

    second commit

--- frontend/src/services/http-common.jsx
+++ frontend/src/services/http-common.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "mekanbul-ten.vercel.app/api ",
+  baseURL: "mekanbul-ten.vercel.app/api",
   headers: {
     Accept: "application/json",
     "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
